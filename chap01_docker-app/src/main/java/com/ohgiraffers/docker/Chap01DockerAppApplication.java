4116aafd517c47bc6673fb6f86d13aa4afae170a
ClownYH <qwer1745@gmail.com>
2024-03-18 20:24:52 +0900

    first commit

---
+++ chap01_docker-app/src/main/java/com/ohgiraffers/docker/Chap01DockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01DockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01DockerAppApplication.class, args);
+    }
+
+}
